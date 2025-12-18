daabe9b2ad03dcaf55ddd68703f4566490a03d57
Null <roitogaming@gmail.com>
2025-12-19 02:06:34 +0900

    Enable Disableメッセージ

--- src/main/java/com/github/senganull/Null_HelloDice_Plugin.java
+++ src/main/java/com/github/senganull/Null_HelloDice_Plugin.java
@@ -7,11 +7,13 @@ public final class Null_HelloDice_Plugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("正常に起動しました。");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("正常に停止しました。");
     }
 }
