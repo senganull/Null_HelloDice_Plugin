3593f437a74c4ee9119286dc898315586e65ca8c
Null <roitogaming@gmail.com>
2025-12-19 02:56:00 +0900

    コードメモ

--- src/main/java/com/github/senganull/Null_HelloDice_Plugin.java
+++ src/main/java/com/github/senganull/Null_HelloDice_Plugin.java
@@ -4,9 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Null_HelloDice_Plugin extends JavaPlugin {
 
+    // @Overrideとは「アノテーション（注釈）」 と呼ばれるもので、Javaコンパイラ（翻訳機）に対する
+    // 「これは親クラスにあるメソッドを上書き（改造）したものですよ」という合図
     @Override
     public void onEnable() {
         // Plugin startup logic
+        // ログには「緊急（Error）」「警告（Warning）」「通常情報（Info）」などのランクがある
         getLogger().info("正常に起動しました。");
 
     }
